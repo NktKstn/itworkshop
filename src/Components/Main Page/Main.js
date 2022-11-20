3abaa7a7778ab1a8b01cb83dbcb33d92ef87ab53
Никита <piknik7473@yandex.ru>
2022-11-20 20:18:44 +0500

    second page

--- src/Components/Main Page/Main.js
+++ src/Components/Main Page/Main.js
@@ -19,7 +19,7 @@ function Main() {
                             </div>
                         </div>
                     </div>
-                    <main>
+                    <main style={{justifyContent:"center", cursor:"pointer", padding:"9em 0 9em 0"}}>
                         <div className="cub-container">
                             <div className="cube-ceil">
                                 <img src="cube-3.gif" alt="cube"/>
@@ -29,6 +29,40 @@ function Main() {
                     <div className="footer">
                         <div className="footer-info">
                             <h1>Ц:\автоматизация \внедрить>снять рутину<span className="low-dash">__</span></h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="ceil">
+                <div className="container">
+                    <div className="header" style={{padding:"20px 20px 20px 10px"}}>
+                        <p style={{fontSize:"52px",}}>ИТ ЦЕХ</p>
+                    </div>
+                    <main style={{justifyContent:"space-between"}}>
+                        <div className="info-about-us">
+                            <p>>автоматизирует <br/> рутинные тех. процессы компаний <br/>в телеграмм-боте </p>
+                            <p>>учет инвентаря\</p>
+                            <p>>учет рабочего времени\</p>
+                            <p>>учет персонала\</p>
+                            <p>>и т.п.> показать на экране <br/>смартфона руководителя\ <span className="low-dash">__</span></p>
+                        </div>
+                        <div className="cub-container" style={{display:"flex", alignItems:"end",paddingTop:"120px"}}>
+                            <div className="cube-ceil">
+                                <img src="cube-3.gif" alt="cube"/>
+                            </div>
+                        </div>
+                    </main>
+                    <div className="footer" style={{justifyContent:"space-between", padding:"3em 0 7em 0"}}>
+                        <div className="bot" style={{alignItems:"center"}}>
+                            <div className="bot-container">
+                                <div className="bot-ceil">
+                                    <img style={{maxWidth: "80px", alignItems:"center"}} src="telegram-2.gif" alt="телеграмм-бот"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                        <h1>Думайте о будущем.</h1>
+                        <h1>Рутина - забота бота ИТ-ЦЕХ</h1>
                         </div>
                     </div>
                 </div>
