966ebae0dbb7ad39b93803af39e4c8b2c6f3f099
Никита <piknik7473@yandex.ru>
2022-11-24 20:09:18 +0500

    deploy

--- src/Components/Main Page/Main.js
+++ src/Components/Main Page/Main.js
@@ -8,8 +8,8 @@ function Main() {
                 <div className="container">
                     <div className="header">
                         <div className="info">
-                            <h1 style={{marginBottom: "20px"}}>ит-цех.рф [версия 1.0]</h1>
-                            <h1>(с) ИТ-ЦЕХ. Все права защищены.</h1>
+                            <h3 style={{marginBottom: "20px"}}>ит-цех.рф [версия 1.0]</h3>
+                            <h3>(с) ИТ-ЦЕХ. Все права защищены.</h3>
                         </div>
                         <div className="bot">
                             <div className="bot-container">
@@ -28,7 +28,7 @@ function Main() {
                     </main>
                     <div className="footer">
                         <div className="footer-info">
-                            <h1>Ц:\автоматизация \внедрить>снять рутину<span className="low-dash">__</span></h1>
+                            <p style={{fontFamily:"'Open Sans', sans-serif", letterSpacing:"2px"}}>Ц:\автоматизация \внедрить>снять рутину<span className="low-dash" style={{color:"black"}}>__</span></p>
                         </div>
                     </div>
                 </div>
@@ -36,15 +36,15 @@ function Main() {
             <div className="ceil">
                 <div className="container container-second-page">
                     <div className="header" style={{padding:"20px 20px 20px 10px"}}>
-                        <p style={{fontSize:"52px",}}>ИТ ЦЕХ</p>
+                        <h1>ИТ-ЦЕХ</h1>
                     </div>
                     <main className="main-second-page">
                         <div className="info-about-us">
-                            <p>>автоматизирует <br/> рутинные тех. процессы компаний <br/>в телеграмм-боте </p>
+                            <p>>автоматизирует <br/>рутинные тех.процессы компаний<br/>в телеграмм-боте </p>
                             <p>>учет инвентаря\</p>
                             <p>>учет рабочего времени\</p>
                             <p>>учет персонала\</p>
-                            <p>>и т.п.> показать на экране <br/>смартфона руководителя\ <span className="low-dash">__</span></p>
+                            <p>>и т.п.>показать на экране<br/>смартфона руководителя\<span className="low-dash" style={{color:"black"}}>__</span></p>
                         </div>
                         <div className="cub-container cub-container-second-page">
                             <div className="cube-ceil cube-ceil-second-page">
@@ -52,17 +52,17 @@ function Main() {
                             </div>
                         </div>
                     </main>
-                    <div className="footer footer-second-page" style={{justifyContent:"space-between", padding:"3em 0 7em 0"}}>
-                        <div className="bot" style={{alignItems:"center"}}>
+                    <div className="footer footer-second-page" style={{justifyContent:"space-between", padding:"5em 0 9em 0"}}>
+                        <div className="bot" style={{alignItems:"center", padding:"10px 20px 20px 10px"}}>
                             <div className="bot-container">
                                 <div className="bot-ceil">
                                     <img style={{maxWidth: "80px", alignItems:"center"}} src="telegram-2.gif" alt="телеграмм-бот"/>
                                 </div>
                             </div>
                         </div>
-                        <div>
-                        <h1>Думайте о будущем.</h1>
-                        <h1>Рутина - забота бота ИТ-ЦЕХ</h1>
+                        <div style={{padding:"0 0 0 10px"}}>
+                        <p>Думайте о будущем.</p>
+                        <p>Рутина - забота бота ИТ-ЦЕХ</p>
                         </div>
                     </div>
                 </div>
